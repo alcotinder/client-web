a36206275f31de0f285bbfa16d39b3af6d924f3c
Andrew <shuganlive@gmail.com>
2019-07-09 18:30:11 +0300

    added sigup

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
 
 const SignUp = () => {
+  const url = "";
+  const [redirect, setRedirect] = useState(false);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setCongirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    console.log(redirect)
+    setRedirect(true);
+    console.log(login, password, email);
+    if (password === confirmPassword) {
+      fetch(url , {
+        method: 'POST',
+        body: JSON.stringify({
+          login,
+          password,
+          email
+        }),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+    } else {
+      alert("Passwords dont match")
+    }
+  }
+  if (redirect) {
+    return <Redirect to="/"/>
+  }
+
   return (
     <div>
-      <h2>SignUp</h2>
+      <p>
+        <label>Login: </label>
+        <input required
+          onChange={({ target }) =>
+            setLogin(target.value)
+          }
+          value={login}
+        />
+      </p>
+      <p>
+        <label>Email: </label>
+        <input type="email" required
+          onChange={({ target }) =>
+            setEmail(target.value)
+          }
+          value={email}
+        />
+      </p>
+      <p>
+        <label>Password: </label>
+        <input type="password" required
+          onChange={({ target }) =>
+            setPassword(target.value)
+          }
+          value={password}
+        />
+      </p>
+      <p>
+        <label>Confirm Password: </label>
+        <input type="password" required
+         onChange={
+           ({target}) => 
+           setCongirmPassword(target.value)
+         }
+         value={confirmPassword}
+        />
+      </p>
+      <p>
+      <button onClick={handleSubmit}>
+        Submit
+      </button>
+      </p>
     </div>
   )
 }
