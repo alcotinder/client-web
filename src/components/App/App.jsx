425e544cd8ae553b701c91a77cadade2c6dda4ab
o1111001 <yaroslavchugai@gmail.com>
2019-07-15 18:00:56 +0300

    Added new routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,44 +1,44 @@
-import React, { useState, useReducer } from 'react'
-import { 
-  BrowserRouter as Router,
-  Route,
-  Switch
+import React, { useState, useReducer, useEffect } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
 } from 'react-router-dom';
 
 import Header from '../Header/Header';
-import Home from '../Home/Home'
+import Home from '../Home/Home';
 import SignIn from '../SignIn/SignIn';
 import SignUp from '../SignUp/SignUp';
-import Profile from '../Profile/ProfilePage'
+import Profile from '../Profile/ProfilePage';
+import AddInfo from '../AddInfo/AddInfo';
 
+import { UserContext } from '../../context';
+import { infoReducer } from '../../reducers';
 
-import {UserContext} from '../../context'
-import {infoReducer} from '../../reducers'
+const inititalState = {
+	protected: '',
+};
 
-const InititalState ={
-  name: "",
-  lastname: "",
-}
+const App = () => {
+	const [state, dispatch] = useReducer(infoReducer, inititalState);
 
+	// useEffect(() => {
+	// }, []);
 
-
-function App () {
-  
-  const [state, dispatch] = useReducer(infoReducer, InititalState)
-
-  return (
-    <UserContext.Provider value={{state, dispatch}}>
-    <Router>
-      <Header />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/profile' component={Profile} />
-        <Route path='/signin' component={SignIn} />
-        <Route path='/signup' component={SignUp} />
-      </Switch>
-    </Router>
-    </UserContext.Provider>
-  );
-}
+	return (
+		<UserContext.Provider value={{ state, dispatch }}>
+			<Router>
+				<Header />
+				<Switch>
+					<Route path='/' exact component={Home} />
+					<Route path='/profile' component={Profile} />
+					<Route path='/signin' component={SignIn} />
+					<Route path='/signup' component={SignUp} />
+					<Route path='/addinfo' component={AddInfo} />
+				</Switch>
+			</Router>
+		</UserContext.Provider>
+	);
+};
 
 export default App;
