7f2b34d9ad0666fac1d6daf532dc16c1205ebac8
Andrew <shuganlive@gmail.com>
2019-07-17 20:28:10 +0300

    info reducrers added

--- src/helpers/reducers.js
+++ src/helpers/reducers.js
@@ -8,6 +8,25 @@ const infoReducer = (state, action) => {
 			city: action.payload.city,
 			drinks: action.payload.drinks,
 		};
+	case 'ADD_PHOTO':
+		return {
+			...state,
+			photo: action.payload
+		}
+	case 'ADD_LOGIN':
+		return {
+			...state,
+			login: action.payload
+		}
+	case 'EDIT_INFO':
+		console.log(action.payload)
+		return {
+			...state,
+			name: action.payload.name,
+			lastname: action.payload.lastname,
+			city: action.payload.city,
+			drinks: action.payload.drinks,
+		}
 	default:
 		return state;
 	}
