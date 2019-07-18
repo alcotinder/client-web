0340116d2d9a9dbbcd594c6129b9ebc0157a9c97
o1111001 <yaroslavchugai@gmail.com>
2019-07-18 16:30:42 +0300

    Create store

---
+++ src/store/reducer.js
@@ -0,0 +1,26 @@
+const infoReducer = (state, action) => {
+	switch (action.type) {
+	case 'ADD_INFO':
+		return {
+			...state,
+			name: action.payload.name,
+			lastname: action.payload.lastname,
+			city: action.payload.city,
+			drinks: action.payload.drinks,
+		};
+	case 'ADD_PHOTO':
+		return {
+			...state,
+			photo: action.payload,
+		};
+	case 'ADD_LOGIN':
+		return {
+			...state,
+			login: action.payload,
+		};
+	default:
+		return state;
+	}
+};
+
+export default infoReducer;
