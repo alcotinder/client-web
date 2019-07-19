71e96e33220d1e88277d8ec592d25453dd59bb09
o1111001 <yaroslavchugai@gmail.com>
2019-07-19 23:15:07 +0300

    Fixed bug loss of state on page reload

--- src/components/AddInfo/AddInfo.jsx
+++ src/components/AddInfo/AddInfo.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { addInfoReq, addPhotoReq } from '../../helpers/apiHelper';
 import { getFromStorage } from '../../utils/storage';
 import { refresh } from '../../services/token.service';
 import getState from '../../services/state.service';
+import { fetchData } from '../../services/user.service';
 
-const AddInfo = () => {
-
+const AddInfo = props => {
   const { state, dispatch } = getState();
   const photo = useRef(null);
 
@@ -13,9 +13,46 @@ const AddInfo = () => {
   const [lastname, setlastname] = useState(state.lastname);
   const [city, setcity] = useState(state.city);
   const [drinks, setdrinks] = useState(state.drinks);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [redirect, setRedirect] = useState(false);
   const [error, setError]  = useState('');
 
-  const uploadPhoto = async() => {
+  useEffect(() => {
+    if (!state.login) {
+      const tokensfromStorage = getFromStorage('tokens');
+      if (tokensfromStorage) {
+        const {
+          accessToken,
+          expiresIn,
+          refreshToken,
+        } = getFromStorage('tokens');
+
+        const updateData = (async() => {
+          setIsLoading(true);
+          if (+expiresIn < +new Date()) {
+            const result = await refresh(refreshToken);
+            result.tokenExpired ? setRedirect(true) : null;
+          }
+          const info = await fetchData(dispatch, accessToken);
+          if (!info) return setRedirect(true);
+          props.updateOnlineStatus(true);
+          props.updateInfo(info);
+          setname(info.name);
+          setlastname(info.lastname);
+          setcity(info.city);
+          setdrinks(info.drinks);
+          setIsLoading(false);
+        })();
+      } else {
+        setRedirect(true);
+      }
+    }
+  }, []);
+
+  const uploadPhoto = async e => {
+    e.preventDefault();
+    setError('');
     const { accessToken, refreshToken, expiresIn } = getFromStorage('tokens');
     const formData = new FormData();
     formData.append('userAvatar', photo.current.files[0]);
@@ -30,6 +67,7 @@ const AddInfo = () => {
 
   const updateInfo = async e => {
     e.preventDefault();
+    setError('');
     const { accessToken, refreshToken, expiresIn } = getFromStorage('tokens');
     if (+expiresIn < new Date()) {
       const tokens = await refresh(refreshToken);
@@ -45,6 +83,9 @@ const AddInfo = () => {
     setError(result.message);
   };
 
+  if (redirect) return <Redirect to='/signin'/>;
+
+  if (isLoading) return <div> Loading... </div>;
   return (
     <div>
       <h1>Add info page</h1>
@@ -52,31 +93,33 @@ const AddInfo = () => {
       <form>
         <p>
           <label>Your photo: </label>
-          <input type="file" ref={photo} />
+          <img src={state.photo} width="200" height="200" />
+          <br />
+          <input type='file' ref={photo} />
           <button onClick={uploadPhoto} >Upload photo</button>
         </p>
         <p>
           <label>Your firstname: </label>
           <input value={name} onChange={e => setname(e.target.value)}
-            placeholder='First name' type='text' name='firstName'required/>
+            placeholder='First name' type='text' name='firstName'/>
         </p>
         <p>
           <label>Your lastname: </label>
           <input value={lastname} onChange={e => setlastname(e.target.value)}
-            placeholder='Last name' type='text' name='lastName'required/>
+            placeholder='Last name' type='text' name='lastName'/>
         </p>
         <p>
           <label>Your city: </label>
           <input value={city} onChange={e => setcity(e.target.value)}
-            placeholder='City' type='text' name='city'required/>
+            placeholder='City' type='text' name='city'/>
         </p>
         <p>
           <label>Your favourite drink: </label>
           <input value={drinks} onChange={e => setdrinks(e.target.value)}
-            placeholder='Favourite drink' type='text' name='drink'required/>
+            placeholder='Favourite drink' type='text' name='drink'/>
         </p>
       </form>
-      <button onClick={updateInfo} >Update info</button>
+      <button onClick={updateInfo}>Update info</button>
     </div>
   );
 };
