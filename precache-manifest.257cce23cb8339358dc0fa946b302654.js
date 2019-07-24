b560fca96e703adf1cb7a4daf6f739219c111c2f
o1111001 <yaroslavchugai@gmail.com>
2019-07-24 22:11:56 +0300

    Updates

---
+++ precache-manifest.257cce23cb8339358dc0fa946b302654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3be28d55b189148821d02b83eed901f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69550d86cb22927458f0",
+    "url": "/static/css/main.1804f342.chunk.css"
+  },
+  {
+    "revision": "8090069bf7298e2bbc21",
+    "url": "/static/js/2.c5e80414.chunk.js"
+  },
+  {
+    "revision": "69550d86cb22927458f0",
+    "url": "/static/js/main.0726ddec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
