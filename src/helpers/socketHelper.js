6fe6f723cafce43ebdb3ac172641502abea653aa
o1111001 <yaroslavchugai@gmail.com>
2019-07-29 23:30:44 +0300

    Connect function

---
+++ src/helpers/socketHelper.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import io from 'socket.io-client';
+
+const url = 'https://localhost:8443';
+
+const connect = accessToken => {
+  const socket = io.connect(url, {
+    query: {
+      authorization: `Bearer ${accessToken}`
+    },
+  });
+
+  return socket;
+};
+
+export { connect };
